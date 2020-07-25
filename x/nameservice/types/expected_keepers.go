606ddf5edf6062de821222a7fa2d0ec0879b99d7
Dmitry Sky <dmitry.sky@icloud.com>
2020-07-25 10:17:58 -1000

    define a Message interface

--- x/nameservice/types/expected_keepers.go
+++ x/nameservice/types/expected_keepers.go
@@ -18,6 +18,30 @@ type BankKeeper interface {
 	SendCoins(ctx sdk.Context, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coins) error
 }
 
+// Transactions messages must fulfill the Msg
+type Msg interface {
+	// Return the message type.
+	// Must be alphanumeric or empty.
+	Type() string
+
+	// Returns a human-readable string for the message, intended for utilization
+	// within tags
+	Route() string
+
+	// ValidateBasic does a simple validation check that
+	// doesn't require access to any other information.
+	ValidateBasic() Error
+
+	// Get the canonical byte representation of the Msg.
+	GetSignBytes() []byte
+
+	// Signers returns the addrs of signers that must sign.
+	// CONTRACT: All signatures must be present to be valid.
+	// CONTRACT: Returns addrs in some deterministic order.
+	GetSigners() []AccAddress
+}
+
+
 /*
 When a module wishes to interact with another module, it is good practice to define what it will use
 as an interface so the module cannot use things that are not permitted.
