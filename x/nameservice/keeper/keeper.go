fa6a39d55a674eeaeb104ccdba795ec5a2cf8eec
Dmitry Sky <dmitry.sky@icloud.com>
2020-07-24 14:12:55 -1000

    Add functions to Get and Set different parameters of Whois

--- x/nameservice/keeper/keeper.go
+++ x/nameservice/keeper/keeper.go
@@ -40,6 +40,52 @@ func (k Keeper) DeleteWhois(ctx sdk.Context, name string) {
 	store.Delete([]byte(name))
 }
 
+// ResolveName - returns the string that the name resolves to
+func (k Keeper) ResolveName(ctx sdk.Context, name string) string {
+	return k.GetWhois(ctx, name).Value
+}
+
+// SetName - sets the value string that a name resolves to
+func (k Keeper) SetName(ctx sdk.Context, name string, value string) {
+	whois := k.GetWhois(ctx, name)
+	whois.Value = value
+	k.SetWhois(ctx, name, whois)
+}
+
+// HasOwner - returns whether or not the name already has an owner
+func (k Keeper) HasOwner(ctx sdk.Context, name string) bool {
+	return !k.GetWhois(ctx, name).Owner.Empty()
+}
+
+// GetOwner - get the current owner of a name
+func (k Keeper) GetOwner(ctx sdk.Context, name string) sdk.AccAddress {
+	return k.GetWhois(ctx, name).Owner
+}
+
+// SetOwner - sets the current owner of a name
+func (k Keeper) SetOwner(ctx sdk.Context, name string, owner sdk.AccAddress) {
+	whois := k.GetWhois(ctx, name)
+	whois.Owner = owner
+	k.SetWhois(ctx, name, whois)
+}
+
+// GetPrice - gets the current price of a name
+func (k Keeper) GetPrice(ctx sdk.Context, name string) sdk.Coins {
+	return k.GetWhois(ctx, name).Price
+}
+
+// SetPrice - sets the current price of a name
+func (k Keeper) SetPrice(ctx sdk.Context, name string, price sdk.Coins) {
+	whois := k.GetWhois(ctx, name)
+	whois.Price = price
+	k.SetWhois(ctx, name, whois)
+}
+
+// Check if the name is present in the store or not
+func (k Keeper) IsNamePresent(ctx sdk.Context, name string) bool {
+	store := ctx.KVStore(k.storeKey)
+	return store.Has([]byte(name))
+}
 
 
 
