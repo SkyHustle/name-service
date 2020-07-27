219d9911f251c52b21df42dd61cefec024bab061
Dmitry Sky <dmitry.sky@icloud.com>
2020-07-26 14:40:36 -1000

    Create alias file to prevent import cycles

---
+++ x/nameservice/alias.go
@@ -0,0 +1,35 @@
+// The main reason for having this file is to prevent import cycles
+package nameservice
+
+import (
+	"github.com/cosmos/sdk-tutorials/nameservice/x/nameservice/keeper"
+	"github.com/cosmos/sdk-tutorials/nameservice/x/nameservice/types"
+)
+
+const (
+	ModuleName   = types.ModuleName
+	RouterKey    = types.RouterKey
+	StoreKey     = types.StoreKey
+	QuerierRoute = types.QuerierRoute
+)
+
+var (
+	NewKeeper        = keeper.NewKeeper
+	NewQuerier       = keeper.NewQuerier
+	NewMsgBuyName    = types.NewMsgBuyName
+	NewMsgSetName    = types.NewMsgSetName
+	NewMsgDeleteName = types.NewMsgDeleteName
+	NewWhois         = types.NewWhois
+	ModuleCdc        = types.ModuleCdc
+	RegisterCodec    = types.RegisterCodec
+)
+
+type (
+	Keeper          = keeper.Keeper
+	MsgSetName      = types.MsgSetName
+	MsgBuyName      = types.MsgBuyName
+	MsgDeleteName   = types.MsgDeleteName
+	QueryResResolve = types.QueryResResolve
+	QueryResNames   = types.QueryResNames
+	Whois           = types.Whois
+)
